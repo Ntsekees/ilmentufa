0511e154d389c9bf938853d3d46ba2c6150e1958
a a <aaa@example.com>
2015-12-10 09:26:01 +0100

    ningau lo pegjese

--- camxes-builder.js
+++ camxes-builder.js
@@ -2,14 +2,23 @@
 var fs = require("fs")
 var PEG = require("pegjs")
 // // read peg and build a parser
-var camxes_peg = fs.readFileSync("\camxes.js.peg").toString();
-var camxes = PEG.buildParser(camxes_peg, {cache: true});
+var camxes_peg = fs.readFileSync("camxes.js.peg").toString();
+try {
+	var camxes = PEG.buildParser(camxes_peg, {
+		cache: true,
+		trace: false,
+		output: "source",
+		allowedStartRules: ["text"],
+	});
+} catch (e) {
+	console.log(JSON.stringify(e));
+	throw e;
+}
 // // write to a file
-// fs.writeFileSync("\camxes.js", camxes.toSource());
-var fd = fs.openSync("\camxes.js", 'w+');
+var fd = fs.openSync("camxes.js", 'w+');
 var buffer = new Buffer('var camxes = ');
 fs.writeSync(fd, buffer, 0, buffer.length);
-buffer = new Buffer(camxes.toSource());
+buffer = new Buffer(camxes);
 fs.writeSync(fd, buffer, 0, buffer.length);
 buffer = new Buffer("\n\nmodule.exports = camxes;\n\nterm = process.argv[2];\nif (term !== undefined && typeof term.valueOf() === 'string')\n  console.log(JSON.stringify(camxes.parse(term)));\n\n");
 fs.writeSync(fd, buffer, 0, buffer.length);
